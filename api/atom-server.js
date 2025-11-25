c05c153dd448e822908af410ac500bc4dfc8cbb4
Adnanone <adnanonescreen@gmail.com>
2025-11-25 16:35:51 +0500

    Update atom-framework to version 1.5.3 in package.json and package-lock.json; enhance error handling in atom-server.js for module resolution

--- api/atom-server.js
+++ api/atom-server.js
@@ -4,4 +4,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 require('../dist/server.js');
 require('../dist/ssr.js');
 
-module.exports = require('../system/runner');
+let runner;
+try {
+    runner = require('../system/runner');
+} catch (localErr) {
+    try {
+        runner = require('atom-framework/system/runner');
+    } catch (pkgErr) {
+        console.error('[ATOM] Unable to resolve system runner from "../system/runner" or "atom-framework/system/runner".');
+        throw pkgErr;
+    }
+}
+
+module.exports = runner;
